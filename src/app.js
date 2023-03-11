f817bdb3ce19dc393a22cfb455accbeb4d0e3922
Luis Sánchez <luis.alberto.sanchez.rios@live.com.mx>
2023-03-10 18:07:56 -0600

    structure

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const {
+  MONGODB_HOST,
+  DBNAME
+} = process.env;
+
+const URI = `${MONGODB_HOST}${DBNAME}`;
+
+mongoose.connect(URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const { connection } = mongoose;
+
+connection.once('open', () => {
+  console.log('DB is connected');
+});
